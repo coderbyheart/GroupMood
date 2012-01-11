d52351fa3c5808cb3d1b42ffd5e99312941925f0
Markus Tacker <m@tacker.org>
2012-01-11 15:45:38 +0000

    equals() und hashCode()

--- moodclient/src/de/hsrm/mi/mobcomp/y2k11grp04/model/BaseModel.java
+++ moodclient/src/de/hsrm/mi/mobcomp/y2k11grp04/model/BaseModel.java
@@ -5,8 +5,8 @@ import android.os.Parcelable;
 
 abstract public class BaseModel implements Parcelable {
 
-	protected int id;
-	protected String creationDate;
+	private int id;
+	private String creationDate;
 
 	@Override
 	public void writeToParcel(Parcel out, int flags) {
@@ -33,6 +33,45 @@ abstract public class BaseModel implements Parcelable {
 
 	public void setCreationDate(String creationDate) {
 		this.creationDate = creationDate;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((creationDate == null) ? 0 : creationDate.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BaseModel other = (BaseModel) obj;
+		if (creationDate == null) {
+			if (other.creationDate != null)
+				return false;
+		} else if (!creationDate.equals(other.creationDate))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
 	}
 
 }
--- moodclient/src/de/hsrm/mi/mobcomp/y2k11grp04/model/Meeting.java
+++ moodclient/src/de/hsrm/mi/mobcomp/y2k11grp04/model/Meeting.java
@@ -79,4 +79,48 @@ public class Meeting extends BaseModel implements StateModel {
 	public void setNumTopics(int numTopics) {
 		this.numTopics = numTopics;
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + numTopics;
+		result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Meeting other = (Meeting) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (numTopics != other.numTopics)
+			return false;
+		if (uri == null) {
+			if (other.uri != null)
+				return false;
+		} else if (!uri.equals(other.uri))
+			return false;
+		return true;
+	}
 }
