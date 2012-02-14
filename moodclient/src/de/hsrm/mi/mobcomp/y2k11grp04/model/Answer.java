afbd4f7a6417285d6dc9370bc0149e1697fdf058
Markus Tacker <m@tacker.org>
2012-02-14 11:18:31 +0000

    Model dazu.

---
+++ moodclient/src/de/hsrm/mi/mobcomp/y2k11grp04/model/Answer.java
@@ -0,0 +1,101 @@
+package de.hsrm.mi.mobcomp.y2k11grp04.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Answer extends BaseModel {
+	private Question question;
+	private String user;
+	private String answer;
+
+	public Answer() {
+	}
+
+	public Answer(Parcel in) {
+		readFromParcel(in);
+	}
+
+	@Override
+	protected void readFromParcel(Parcel in) {
+		super.readFromParcel(in);
+		user = in.readString();
+		answer = in.readString();
+	}
+
+	@Override
+	public void writeToParcel(Parcel out, int flags) {
+		super.writeToParcel(out, flags);
+		out.writeString(user);
+		out.writeString(answer);
+	}
+
+	public Question getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	public static final Parcelable.Creator<Answer> CREATOR = new Parcelable.Creator<Answer>() {
+		public Answer createFromParcel(Parcel in) {
+			return new Answer(in);
+		}
+
+		public Answer[] newArray(int size) {
+			return new Answer[size];
+		}
+	};
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String key) {
+		this.user = key;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(String value) {
+		this.answer = value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		result = prime * result + ((answer == null) ? 0 : answer.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Answer other = (Answer) obj;
+		if (user == null) {
+			if (other.user != null)
+				return false;
+		} else if (!user.equals(other.user))
+			return false;
+		if (question == null) {
+			if (other.question != null)
+				return false;
+		} else if (!question.equals(other.question))
+			return false;
+		if (answer == null) {
+			if (other.answer != null)
+				return false;
+		} else if (!answer.equals(other.answer))
+			return false;
+		return true;
+	}
+}
