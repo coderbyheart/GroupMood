0ed05e83da8994fffe373e0523114385913d6b16
Markus Tacker <m@tacker.org>
2012-02-21 15:17:30 +0000

    Korrektes Runden (problem bei kleinen Ranges).

--- moodclient/src/de/hsrm/mi/mobcomp/y2k11grp04/gui/TopicResultAdapter.java
+++ moodclient/src/de/hsrm/mi/mobcomp/y2k11grp04/gui/TopicResultAdapter.java
@@ -3,7 +3,6 @@ package de.hsrm.mi.mobcomp.y2k11grp04.gui;
 import java.util.List;
 
 import android.content.res.Resources;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
